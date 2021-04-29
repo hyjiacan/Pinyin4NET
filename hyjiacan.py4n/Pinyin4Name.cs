fb29c073a269228ac98eca3e9ec31d5be5c88e9d
hyjiacan <hyjiacan@163.com>
2021-04-29 18:01:10 +0800

    调整一些注释

--- hyjiacan.py4n/Pinyin4Name.cs
+++ hyjiacan.py4n/Pinyin4Name.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace hyjiacan.py4n
 {
-    [System.Obsolete("此接口已经禁用请改用 Name4Net 代替")]
+    [System.Obsolete("此接口已经不再建议使用，请改用 Name4Net 代替。此接口将在 5.0 版本被移除")]
     public class Pinyin4Name: Name4Net
     {
         private Pinyin4Name() {}
